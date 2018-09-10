642daa6a8eeec67937a759bc5008e19f073bae02
newexod <dimapulenko@gmail.com>
2018-09-10 16:37:18 +0300

    Lists & State

--- src/App.js
+++ src/App.js
@@ -14,18 +14,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = 'Dmitry';
-
-    this.setState({
-      persons: [
-        { name: newName, age: 25 },
-        { name: "Yura", age: 20 },
-        { name: "Kolya", age: 40 }
-      ]
-    })
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -43,6 +31,16 @@ class App extends Component {
       showPersons: !doesShow
     })
   }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+
+    persons.splice(personIndex, 1);
+
+    this.setState({
+      persons: persons
+    });
+  }
   
   render() {
     const styleBtn = {
@@ -58,8 +56,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
+              click={ () => this.deletePersonHandler(index) }
               name={person.name} 
               age={person.age} 
             />
