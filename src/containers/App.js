728f31ef5522b1b2d40f4f8ccc0e8868d0e0f86a
newexod <dimapulenko@gmail.com>
2018-09-12 18:22:49 +0300

    Updating Lifecycle Hooks (triggered by State Changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,6 +27,19 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate()', nextProps, nextState);    
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);    
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate()');    
+  }
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(person => {
       return person.id === id
