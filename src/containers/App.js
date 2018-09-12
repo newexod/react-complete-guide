43209bedeb73bf4d14ad33d7fa1753c4a466224f
newexod <dimapulenko@gmail.com>
2018-09-12 16:07:08 +0300

    Splitting an App Into Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -54,44 +55,24 @@ class App extends Component {
   
   render() {
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-                click={ () => this.deletePersonHandler(index) }
-                name={person.name} 
-                age={person.age} 
-                key={person.id}
-                changed={ (event) => this.nameChangedHandler(event, person.id) }
-              />
-          })}
-        </div>
+        <Persons 
+          persons={this.state.persons}
+          changed={this.nameChangedHandler} 
+          clicked={this.deletePersonHandler} 
+        />
       );
-
-      btnClass = classes.Red;
-    }
-
-    // let classes = ['red', 'bold'].join(' '); // class="red bold"
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
       <div className={classes.App}>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button 
-          className={btnClass}
-          onClick={this.togglePersonsHandler}
-        >
-          Toggle Persons
-        </button>
+        <Cockpit 
+          persons={this.state.persons}
+          showPersons={this.state.showPersons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
