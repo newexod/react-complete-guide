ff81ee549f45ca24d99726e1fca394eca485768a
newexod <dimapulenko@gmail.com>
2018-09-13 19:26:19 +0300

    Understanding Higher Order Components (HOCs)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import classes from './Cockpit.css';
+import Aux from '../../hoc/ReactAux';
 
 const cockpit = (props) => {
   const assignedClasses = [];
-  let btnClass = '';
+  let btnClass = classes.Button;
 
   if (props.showPersons) {
-    btnClass = classes.Red;
+    btnClass = [classes.Button, classes.Red].join(' ');
   }
 
   if (props.persons.length <= 2) {
@@ -18,7 +19,7 @@ const cockpit = (props) => {
   }
 
   return (
-    <div className={classes.Cockpit}>
+    <Aux>
       <h1>{props.appTitle}</h1>
       <p className={assignedClasses.join(' ')}>This is really working!</p>
       <button 
@@ -27,7 +28,7 @@ const cockpit = (props) => {
       >
         Toggle Persons
       </button>
-    </div>
+    </Aux>
   );
 };
 
